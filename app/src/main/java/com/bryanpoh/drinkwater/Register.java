d406c537930473ee687b20b02c624414555bc52d
Bryan Poh <zandrex00@gmail.com>
2020-05-03 23:11:00 +0800

    fixed logout and backbutton

--- app/src/main/java/com/bryanpoh/drinkwater/Register.java
+++ app/src/main/java/com/bryanpoh/drinkwater/Register.java
@@ -101,9 +101,11 @@ public class Register extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if(task.isSuccessful()){
-                                        Intent intent = new Intent(Register.this, MainActivity.class);
+                                        Intent intent = new Intent(Register.this, Login.class);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                                         startActivity(intent);
+
+                                        Toast.makeText(Register.this, "Account Created. Please Login", Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
