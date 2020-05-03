9a40daf2a65b25defb869965600ffab9ba79684c
Bryan Poh <zandrex00@gmail.com>
2020-05-03 21:28:27 +0800

    Timer and Notification done

--- app/src/main/java/com/bryanpoh/drinkwater/MainActivity.java
+++ app/src/main/java/com/bryanpoh/drinkwater/MainActivity.java
@@ -1,9 +1,17 @@
 package com.bryanpoh.drinkwater;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -24,16 +32,14 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvDate, tvIntakeGoal, tvIntakePercent;
+    TextView tvDate, tvIntakeGoal, tvIntakePercent, tvReminder;
     Button drinkBtn;
     ImageButton settingsBtn;
     ProgressBar intakeProgressBar;
-
-    private int currentProgress = 0;
-    private String currProgressPercent;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("Users");
@@ -41,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
 
     // Global var for user data in this activity
     String _USERID, _USERNAME, _EMAIL, _DRINKSIZE, _BOTTLESIZE;
+
+    // How much user drank
+    int currentProgress = 0;
+
+    // String value for progress %
+    String currProgressPercent;
+
+    // Notification
+    NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +68,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        notificationManager = NotificationManagerCompat.from(MainActivity.this);
+
         tvDate = findViewById(R.id.tvDate);
+        tvReminder = findViewById(R.id.tvTime);
         tvIntakePercent = findViewById(R.id.tvIntakePercent);
         tvIntakeGoal = findViewById(R.id.tvIntakeGoal);
         drinkBtn = findViewById(R.id.btnDrink);
@@ -62,27 +80,65 @@ public class MainActivity extends AppCompatActivity {
 
         showUserData();
 
+        /*// Notification
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+        String notiMsg = "Reminder to stay hydrated! Drink water now!";
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this)
+                .setSmallIcon(R.drawable.ic_notificiation_bell)
+                .setContentTitle("New Reminder!")
+                .setContentText(notiMsg)
+                .setContentIntent(pendingIntent) // Set where user goes when tap
+                .setAutoCancel(true) // Removes noti after user tap
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+         notificationManager = NotificationCompat.from(MainActivity.this);*/
+
+        //
+
         String date = new SimpleDateFormat("EEE, dd MMM yyyy", Locale.getDefault()).format(new Date());
+
+        // Countdown timer for 1 hour
+        new CountDownTimer(3600000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                long millis = millisUntilFinished;
+                //Convert milliseconds into hour,minute and seconds
+                String hms = String.format("%02d:%02d:%02d",
+                        TimeUnit.MILLISECONDS.toHours(millis),
+                        TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)), TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+                tvReminder.setText(hms);//set text
+            }
+
+            public void onFinish() {
+                showNotification();
+            }
+        }.start();
+        //
+
 
 
         tvDate.setText(date);
-        intakeProgressBar.setMax(100);
+
+        // Set progress
         intakeProgressBar.setProgress(currentProgress);
-        currProgressPercent = currentProgress + "%";
-        tvIntakePercent.setText(currProgressPercent);
-
-
+        String str_currentProgress = currentProgress + "ml";
+        tvIntakePercent.setText(str_currentProgress);
 
         drinkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentProgress += 25;
+
+                // Add drink to current that user already drank
+                currentProgress += Integer.parseInt(_DRINKSIZE);
+
+                // Set progress bar with current progress
                 intakeProgressBar.setProgress(currentProgress);
-                currProgressPercent = currentProgress + "%";
-                if(currentProgress < 100)
-                {
-                    tvIntakePercent.setText(currProgressPercent);
-                }
+
+                String str_currentProgress = currentProgress + "ml";
+                tvIntakePercent.setText(str_currentProgress);
             }
         });
 
@@ -93,6 +149,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void showNotification(){
+        String notiMsg = "Reminder to stay hydrated! Drink water now!";
+//
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_REMINDER_ID)
+                .setSmallIcon(R.drawable.ic_notificiation_bell)
+                .setContentTitle("Alert from DrinkWater!")
+                .setContentText(notiMsg)
+                .setContentIntent(pendingIntent) // Set where user goes when tap
+                .setAutoCancel(true) // Removes noti after user tap
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+
+        notificationManager.notify(1, notification);
     }
 
     private void showUserData(){
@@ -136,9 +211,23 @@ public class MainActivity extends AppCompatActivity {
                         _EMAIL = email;
                         _DRINKSIZE = drinkSize;
                         _BOTTLESIZE = bottleSize;
-                        tvIntakeGoal.setText(_BOTTLESIZE);
+                        tvIntakeGoal.setText(_BOTTLESIZE + "ml");
                     }
                 }
+
+                // Set max value of progress bar: bottleSize
+                intakeProgressBar.setMax(Integer.parseInt(_BOTTLESIZE));
+                Log.d("tag", "After bottle" + Integer.parseInt(_BOTTLESIZE));
+
+                // Calculate each drink percent out of bottesize
+                //currentProgress = Integer.parseInt(_DRINKSIZE) / Integer.parseInt(_BOTTLESIZE);
+                Log.d("tag", "CURRENT PROGRESS INSIDE DS: " + currentProgress);
+
+
+                //Log.d("tag", "bottle size: " + Integer.parseInt(_BOTTLESIZE));
+
+                //currProgressPercent = (currentProgress * 100) + "%";
+                //tvIntakePercent.setText(currProgressPercent);
             }
 
             @Override
@@ -146,8 +235,6 @@ public class MainActivity extends AppCompatActivity {
                 // Failed to read value
                 Log.w("tag", "Failed to read value.", error.toException());
             }
-
-
         });
     }
 }
